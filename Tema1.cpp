9f8c749e178f8463559e00f7168815120c185fa3
pavelusistu <63097668+pavelusistu@users.noreply.github.com>
2020-11-16 21:02:36 +0200

    Update Tema1.cpp

--- Tema1.cpp
+++ Tema1.cpp
@@ -111,7 +111,18 @@ public:
     void afisare(ostream &out);
     friend istream& operator>>(istream &in, clasa &x);
     friend ostream& operator<<(ostream &out, clasa &x);
+    void situatie();
 };
+
+void clasa::situatie()
+{
+    for(int i=0; i<n; i++)
+    {
+        c[i].promovabilitate();
+        c[i].performanta();
+    }
+
+}
 
 clasa::clasa()
 
@@ -163,6 +174,10 @@ void clasa::citire(istream &in)
     in>>specializare;
     cout<<"Sala: ";
     in>>sala;
+    for(int i=0; i<n; i++)
+    {
+        c[i].citire(in);
+    }
 
 }
 
@@ -181,12 +196,19 @@ clasa& clasa::operator=(const clasa &x)
     specializare=x.specializare;
     sala=x.sala;
     n=x.n;
+    return *this;
 }
 
 void clasa::afisare(ostream &out)
 
 {
-    out<<n<<" "<<numar<<" "<<litera<<" "<<specializare<<" "<<sala<<"\n";
+    out<<"Datele clasei sunt: ";
+    out<<numar<<" "<<litera<<" "<<specializare<<" "<<sala<<"\n";
+    out<<"Elevii clasei sunt: "<<endl;
+    for(int i=0; i<n; i++)
+    {
+        c[i].afisare(out);
+    }
 }
 ostream& operator<<(ostream& out, clasa& x)
 {
@@ -199,23 +221,9 @@ ostream& operator<<(ostream& out, clasa& x)
 int main()
 {
     clasa p;
-    int n;
     cout<<"Cititi datele clasei"<<endl;
     cin>>p;
     cout<<p;
-    cout<<"Cititi numarul de elevi din clasa ";
-    cin>>n;
-    cout<<"Numarul elevilor din clasa este: "<<n<<endl;
-    elev e[n];
-    cout<<"Cititi datele elevilor: "<<endl;
-    for(int i=0; i<n; i++)
-        cin>>e[i];
-    for(int i=0; i<n; i++)
-        cout<<e[i];
-    for(int i=0; i<n; i++)
-    {
-        e[i].promovabilitate();
-        e[i].performanta();
-    }
+    p.situatie();
     return 0;
 }
